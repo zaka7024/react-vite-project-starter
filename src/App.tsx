4d4128727c02d961f2265c9250284eee483e88b3
Ahmad <zaka7024@users.noreply.github.com>
2025-12-10 18:18:12 +0300

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/home'
 
 function App() {
 
   return (
-    <>
-      <p>Your App Will Appear Here Once Ready</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/home.tsx
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+  return (
+    <div>Your App Will Appear Here Once Ready</div>
+  )
+}
+
+export default HomePage
